2a72fb92f35bf14b5ade1835547dfbd29f5181da
Sam Robbins <samrobbinsgb@gmail.com>
2020-07-02 16:01:39 +0100

    Add initial files from Tailwind Starter

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Nav from "../components/nav";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Nav />
+      <div className="hero">
+        <h1 className="title">Notes Site</h1>
+      </div>
+    </div>
+  );
+}
