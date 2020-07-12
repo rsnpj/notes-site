f3ba192d61735c8a4a4a8127900b56a582376855
Sam Robbins <samrobbinsgb@gmail.com>
2020-07-12 16:31:10 +0100

    CSS part of sidebar/content relation complete

---
+++ components/new_sidebar.js
@@ -0,0 +1,12 @@
+function Sidebar() {
+  return (
+    <>
+      <div className="absolute sm:relative sm:flex-none w-full max-w-xs text-black bg-white overflow-y-auto p-4 border-r h-full overflow-x-hidden">
+        <h1>Hello</h1>
+        <hr className="mb-4" />
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import NavBar from "../components/navbar";
+import Sidebar from "../components/new_sidebar";
 function HomePage() {
   return (
     <>
       <NavBar />
-      <div>Welcome to Next.js!</div>
+      <div className="sm:flex main-content">
+        <div className="absolute sm:relative w-full max-w-xs z-10 main-content">
+          <Sidebar />
+        </div>
+        <div className=" z-0 sm:flex-1 bg-white main-content p-4">
+          Welcome to Next.js! Here is some more content to check if it properly
+          underneath
+        </div>
+      </div>
     </>
   );
 }
