865933fe5dc74823917b887d1faf62da53d28659
Sam Robbins <samrobbinsgb@gmail.com>
2020-07-12 11:02:18 +0100

    Start fresh on index page - add navbar

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import NavBar from "../components/navbar";
+function HomePage() {
+  return (
+    <>
+      <NavBar />
+      <div>Welcome to Next.js!</div>
+    </>
+  );
+}
+
+export default HomePage;
--- pages/index.md
+++
@@ -1,5 +0,0 @@
----
-title: "Homepage"
----
-
-This website contains all my notes from my course at Durham University
