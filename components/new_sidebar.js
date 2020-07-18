278825fa8074957eeede48bc34a44d7a4c825fd2
Sam Robbins <samrobbinsgb@gmail.com>
2020-07-18 10:42:30 +0100

    Start of dismissing sidebar

--- components/new_sidebar.js
+++ components/new_sidebar.js
@@ -4,6 +4,7 @@ function Sidebar(props) {
   return (
     <div
       className={`${toggle} absolute sm:relative w-full max-w-xs z-10 main-content`}
+      ref={props.node}
     >
       <div className="text-black bg-white overflow-y-auto p-4 border-r h-full overflow-x-hidden">
         <h1>Hello</h1>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import NavBar from "../components/navbar";
 import Sidebar from "../components/new_sidebar";
 import MainContent from "../components/main_content";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function HomePage() {
   const [sidebarVisible, setSidebarVisible] = useState(false);
@@ -9,11 +9,34 @@ function HomePage() {
     setSidebarVisible(!sidebarVisible);
   }
 
+  useEffect(() => {
+    if (open) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+  const node = useRef();
+  const node2 = useRef();
+  const handleClickOutside = (e) => {
+    console.log("clicking anywhere");
+    if (node.current.contains(e.target)) {
+      // inside click
+      return;
+    }
+    // outside click
+    setSidebarVisible(false);
+  };
+
   return (
     <>
       <NavBar toggleFunction={toggleSidebar} />
       <div className="sm:flex main-content">
-        <Sidebar toggle={sidebarVisible} />
+        <Sidebar toggle={sidebarVisible} ref={node} />
         <MainContent toggle={sidebarVisible}>
           Welcome to Next.js! Here is some more content to check if it is
           properly underneath
