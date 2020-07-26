b781fd96b61adb268cb90c9245c12ae10973a3cd
Sam Robbins <samrobbinsgb@gmail.com>
2020-07-26 18:41:14 +0100

    Start work with sidebar

--- components/new_sidebar.js
+++ components/new_sidebar.js
@@ -1,13 +1,24 @@
+import Select from "react-select";
 const Sidebar = React.forwardRef((props, ref) => {
   const toggle = props.toggle ? "" : "hidden sm:block";
+  var options = [
+    { value: "year_1", label: "Year 1" },
+    { value: "year_2", label: "Year 2" },
+  ];
   return (
     <div
       className={`${toggle} absolute sm:relative w-full max-w-xs z-10 main-content`}
       ref={ref}
     >
       <div className="h-full p-4 overflow-x-hidden overflow-y-auto text-black bg-white border-r">
-        <h1>Hello</h1>
-        <hr className="mb-4" />
+        <Select
+          options={options}
+          // onChange={handleChange}
+          isClearable={false}
+          isSearchable={false}
+          instanceId={1}
+        />
+        <hr className="mt-4 mb-4" />
       </div>
     </div>
   );
