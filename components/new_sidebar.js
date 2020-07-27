81e9443458b9f463128c43e2ba08fe14d105688b
Sam Robbins <samrobbinsgb@gmail.com>
2020-07-27 20:25:42 +0100

    Start work on selecting modules

--- components/new_sidebar.js
+++ components/new_sidebar.js
@@ -3,12 +3,14 @@ import { useState } from "react";
 const Sidebar = React.forwardRef((props, ref) => {
   const [year, setYear] = useState("");
   const [modules, setModules] = useState([]);
+  const [module, setModule] = useState(false);
   const toggle = props.toggle ? "" : "hidden sm:block";
   var options = props.tree.children.map((x) => ({
     value: x.name,
     label: x.name.replace(/_/g, " "),
   }));
   function handleChange(selected) {
+    setModule(false);
     setYear(selected.value);
     console.log(props.tree);
     setModules(
@@ -17,6 +19,33 @@ const Sidebar = React.forwardRef((props, ref) => {
         .children.map((x) => x.name)
     );
   }
+
+  function Module_layer() {
+    return (
+      <ul>
+        {modules.map((element) => (
+          <li key={element}>
+            <button onClick={() => setModule(element)}>
+              {element.replace(/_/g, " ")}
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  function Submodule_layer() {
+    return "Hiya";
+  }
+
+  function Switching() {
+    if (module) {
+      return <Submodule_layer />;
+    } else {
+      return <Module_layer />;
+    }
+  }
+
   return (
     <div
       className={`${toggle} absolute sm:relative w-full max-w-xs z-10 main-content`}
@@ -31,11 +60,7 @@ const Sidebar = React.forwardRef((props, ref) => {
           instanceId={1}
         />
         <hr className="mt-4 mb-4" />
-        <ul>
-          {modules.map((element) => (
-            <li key={element}>{element}</li>
-          ))}
-        </ul>
+        <Switching />
       </div>
     </div>
   );
