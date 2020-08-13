fabc9b1d28e522b95b13d70ab572fd0999f4d977
Sam Robbins <samrobbinsgb@gmail.com>
2020-08-13 16:37:31 +0100

    Fix error but everything seems to be working!

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -40,7 +40,7 @@ function Lecture({ tree, postData, params }) {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
           integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
       </Head>
       <NavBar toggleFunction={toggleSidebar} ref={node2} />
