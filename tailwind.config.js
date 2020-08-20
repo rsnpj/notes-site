bbb820b4cbd8de992b89f6942ec01557b514e00e
Sam Robbins <samrobbinsgb@gmail.com>
2020-08-20 18:39:36 +0100

    Update tailwind config to include lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./layouts/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
