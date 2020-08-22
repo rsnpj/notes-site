3e85e4c82f919dd2d083d89b75020076952e58e0
Sam Robbins <samrobbinsgb@gmail.com>
2020-08-22 11:25:22 +0100

    Add markdown files to purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./layouts/**/*.{js,ts,jsx,tsx}",
     "./lib/**/*.{js,ts,jsx,tsx}",
+    "./notes/**/*.{md}",
   ],
   theme: {
     extend: {
