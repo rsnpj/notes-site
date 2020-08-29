0c6cc8ed569ca4b01f6a69ca8f05a524ac323891
Sam Robbins <samrobbinsgb@gmail.com>
2020-08-29 14:40:16 +0100

    Add adsense script

--- pages/_document.js
+++ pages/_document.js
@@ -27,6 +27,11 @@ class MyDocument extends Document {
           `,
 						}}
 					/>
+					<script
+						data-ad-client="ca-pub-2469065497002287"
+						async
+						src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+					></script>
 				</Head>
 				<body>
 					<Main />
