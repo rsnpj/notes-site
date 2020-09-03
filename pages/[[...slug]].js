c3659062befed07c03ee82e4fc967509ee967dea
Sam Robbins <samrobbinsgb@gmail.com>
2020-09-03 19:03:23 +0100

    Change style of anchor links

--- pages/[[...slug]].js
+++ pages/[[...slug]].js
@@ -93,10 +93,7 @@ function Lecture({ tree, postData, params }) {
 					{!postData.isHome && (
 						<>
 							<div className="p-6 pt-24 pb-12">
-								<h1
-									className="text-4xl sm:text-6xl text-center font-semibold"
-									id="title"
-								>
+								<h1 className="text-4xl sm:text-6xl text-center font-semibold title">
 									{postData.title}
 								</h1>
 								<h2 className="text-center text-lg text-gray-700">
