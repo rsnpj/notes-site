2477f819c259dcdf0384b00319bb0fe4650b5bc4
Sam Robbins <samrobbinsgb@gmail.com>
2020-09-30 13:33:51 +0100

    Add alt to diagrams

--- components/mdx/code.js
+++ components/mdx/code.js
@@ -5,7 +5,7 @@ const pako = require("pako");
 export default function MyCode(props) {
 	if (props.className === "language-mermaid") {
 		var result = base64.encode(props.children);
-		return <img src={"https://mermaid.ink/img/" + result} />;
+		return <img src={"https://mermaid.ink/img/" + result} alt="Diagram" />;
 	} else if (props.className === "language-graphviz") {
 		var bytes = utf8.encode(props.children);
 		const compressed = pako.deflate(bytes, { level: 9, to: "string" });
@@ -13,7 +13,12 @@ export default function MyCode(props) {
 			.encode(compressed)
 			.replace(/\+/g, "-")
 			.replace(/\//g, "_");
-		return <img src={"https://kroki.io/graphviz/svg/" + result} />;
+		return (
+			<img
+				src={"https://kroki.io/graphviz/svg/" + result}
+				alt="Diagram"
+			/>
+		);
 	} else {
 		return <code {...props} />;
 	}
