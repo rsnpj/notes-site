bd164ce921ba945d4133d3adaf6f8163dd80e4d2
Sam Robbins <samrobbinsgb@gmail.com>
2020-10-21 11:43:43 +0100

    Better fix to layout shift

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -150,7 +150,7 @@ function Lecture({
 							</a>
 						</h3>
 					)}
-					<div className="flex justify-center pt-2 h-32">
+					<div className="flex justify-center pt-2 h-40">
 						<InnerHTML html={html} />
 					</div>
 				</div>
